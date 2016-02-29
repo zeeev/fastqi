cb04873e87ee1a79cc1998c44d5f2442384602af
Zev N. Kronenberg <zevk@e162.grid.gs.washington.edu>
2016-02-29 15:09:56 -0800

    new test function

--- test/bloomHandlerTest.h
+++ test/bloomHandlerTest.h
@@ -42,3 +42,57 @@ TEST(bloomIO, read_write){
   }  
 };
 
+TEST(bloomIO, findTPs){
+  
+  bloom_parameters parameters;
+  parameters.projected_element_count    = 10000;
+  parameters.false_positive_probability = 0.001;
+  parameters.random_seed                =     1;
+  parameters.compute_optimal_parameters()      ;
+  
+  bloomContainer created_blooms;
+  
+  std::vector<uint64_t> inserted;
+  
+  for(int i = 0; i < 10; i++){
+    created_blooms.add(parameters, 0);
+    for(int j = 0 ; j < 100; j++){
+      uint64_t r = rand();
+      created_blooms.data[i]->bf.insert(r);
+      inserted.push_back(r);
+    }
+  }
+
+  std::string test = "test-index2.bin";
+  {
+    bloomIO controller(test);
+    controller.openForWrite();
+    controller.write(created_blooms);
+  }
+  
+  bloomContainer loaded_blooms;
+  {
+    bloomIO controller(test);
+    controller.openForRead();
+    controller.read(loaded_blooms);
+  }
+  for(std::vector<uint64_t>::iterator it = inserted.begin();
+      it != inserted.end(); it++){
+    
+    bool found = false ;
+    bool answer = true;
+    
+    
+    for(std::vector<bloomWrapper *>::iterator iz
+	  = loaded_blooms.data.begin();
+	iz != loaded_blooms.data.end(); iz++){
+      
+      if((*iz)->bf.contains(*it)){
+	found = true;
+	break;
+      }
+    }
+    ASSERT_EQ(found, answer);
+  }
+};
+
